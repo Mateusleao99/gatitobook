a9fec4a2697d28fcccaaaae209035afbf700264b
Mateus Leao <mateus.san.tos@hotmail.com>
2021-09-09 07:15:20 -0300

    feat: cadastrando usuario na api

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,8 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuarioService } from './../../services/registrar/novo-usuario.service';
 import { UsuarioExisteService } from './../../services/validacao-usuario/usuario-existe.service';
 import { NovoUsuario } from './../../interfaces/novo-usuario';
-import { NovoUsuarioService } from './../../services/registrar/novo-usuario.service';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { minusculoValidator } from './minusculo.validator';
 import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
@@ -17,14 +18,15 @@ export class NovoUsuarioComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private registrarService: NovoUsuarioService,
-    private usuarioExisteService: UsuarioExisteService
+    private usuarioExisteService: UsuarioExisteService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group(
       {
         email: ['', [Validators.required, Validators.email]],
-        fullName: ['', [Validators.required, Validators.minLength(40)]],
+        fullName: ['', [Validators.required, Validators.minLength(10)]],
         userName: [
           '',
           [minusculoValidator],
@@ -39,7 +41,15 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if(this.novoUsuarioForm.valid){
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.registrarService.cadastraNovoUsuario(novoUsuario).subscribe(()=>{
+        this.router.navigate(['']);
+      },
+      (error)=> {
+        console.log(error);
+      }
+      )
+    }
   }
 }
